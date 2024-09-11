3ddc77ed9e7783c74698089d646480f52275de8d
Jullian York G. Santos <qjygsantos@tip.edu.ph>
2024-09-11 17:44:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ class InferenceClient:
                 'coordinates': (x, y),
                 'height': height,
                 'width': width,
-                'command': matched_command if text != "No text detected" else "",
+                'command': matched_command if text != "no text detected" else "",
                 'pos': pos,
                 'elbow_top_left': False,  # Default to False
                 'elbow_bottom_curved': False,
