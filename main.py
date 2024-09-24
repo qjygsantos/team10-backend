01d131ce88bf17963f217b9589a7a6cfc02cfba5
Jullian York G. Santos <qjygsantos@tip.edu.ph>
2024-09-24 20:33:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,20 @@ for directory in ['static/objects', 'static/detected_images']:
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-# Set environment variables for credentials
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/google-vision-config.json"  # For Google Cloud Vision API
+# Create JSON files from environment variables
+google_credentials_json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+firebase_credentials_json = os.environ.get("FIREBASE_APPLICATION_CREDENTIALS_JSON")
+
+if google_credentials_json:
+    with open("/app/google-vision-config.json", "w") as google_file:
+        json.dump(json.loads(google_credentials_json), google_file)
+
+if firebase_credentials_json:
+    with open("/app/firebase-config.json", "w") as firebase_file:
+        json.dump(json.loads(firebase_credentials_json), firebase_file)
+
+# Set environment variable for Google Cloud Vision API
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/app/google-vision-config.json"
 
 app = FastAPI()
 
@@ -38,14 +50,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/models", StaticFiles(directory="models"), name="models")
 templates = Jinja2Templates(directory="templates")
 
-
 # Initialize Firebase Admin
 cred = credentials.Certificate("/app/firebase-config.json")
 firebase_admin.initialize_app(cred, {'storageBucket': 'psykitz-891d8.appspot.com'})
 
 db = firestore.client()
 bucket = storage.bucket()
-
 predefined_commands = [
     "move forward",
     "move forward two times",
