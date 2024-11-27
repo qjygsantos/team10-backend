6b19c7edee0a8eabef6db02b55c0e776fc1d829f
Jullian York G. Santos <qjygsantos@tip.edu.ph>
2024-11-27 14:44:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1067,6 +1067,7 @@ def validate_pseudocode(pseudocode: str):
     def generate_error(line_no, line_text, description):
         return {
             "status": "fail",
+            "line_with_error": line_text
             "error_message": f"""
 line {line_no}
     {line_text}
@@ -1306,6 +1307,7 @@ async def translate_pseudocode_from_file(file: UploadFile):
             "status": "Fail",
             "message": "Uh oh, there's an error in your pseudocode, tap the error symbol on the top-right corner to check which caused it.",
             "error_message": validation_result["error_message"],
+            "highlighted_line": validation_result["line_with_error"]
             "arduino_commands": ""  # Return an empty string if validation fails
         }
 
